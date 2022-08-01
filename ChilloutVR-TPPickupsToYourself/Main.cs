f9e9bd6125b2991193c7af9d1bd764ea67770287
Arsinnn <90065236+Arsinnn@users.noreply.github.com>
2022-08-01 19:45:13 -0400

    Add project files.

---
+++ ChilloutVR-TPPickupsToYourself/Main.cs
@@ -0,0 +1,22 @@
+﻿using MelonLoader;
+using UnityEngine;
+using ABI.CCK.Components;
+
+namespace ChilloutVR_TPPickupsToYourself
+{
+    public class Main : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if (Input.GetKey(KeyCode.Y))
+            {
+                var Pickups = Resources.FindObjectsOfTypeAll<CVRPickupObject>();
+                foreach (var item in Pickups)
+                {
+                    item.GetComponent<CVRPickupObject>().grabbedBy = GameObject.Find("_PLAYERLOCAL/Voice Centralizer").GetComponent<Dissonance.Integrations.DarkRift2.DarkRift2Player>().PlayerId;
+                    item.transform.position = GameObject.Find("_PLAYERLOCAL").transform.position;
+                }
+            }
+        }
+    }
+}
---
+++ ChilloutVR-TPPickupsToYourself/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyProduct("Joegers")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("Joegers")]
+[assembly: AssemblyFileVersion("4.9.0")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: MelonGame(null, null)]
+[assembly: AssemblyTitle("Joegers")]
+[assembly: AssemblyDescription("")]
+[assembly: MelonInfo(typeof(ChilloutVR_TPPickupsToYourself.Main), "Joegers", "1.0.0", "arsin#9999", null)]
+[assembly: Guid("9a825bf4-2fba-44cc-81c8-930834c8bcda")]
